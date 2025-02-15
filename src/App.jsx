96459225732771a410458b820d4cbfc2fffac743
dominiccash44@gmail.com <dominiccash44@gmail.com>
2025-02-15 13:15:49 +0000

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
